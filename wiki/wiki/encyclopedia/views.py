16b2caafe5e941f7432ec5fc8b6b9c234c2bd295
thiagoraquel <thiaguinho2005@yahoo.com.br>
2026-02-21 17:53:22 -0300

    começo de entrys

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    conteudo = util.get_entry(title)
+
+    if conteudo is None:
+        return render(request, "encyclopedia/error.html", {
+            "message": "A página '{title}' não foi encontrada."
+        })
+    
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "conteudo": conteudo
+    })
